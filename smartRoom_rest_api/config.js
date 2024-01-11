6a6895d654bca57a1dea54a4fb75af6a483bc109
springnow <150224684+springeun@users.noreply.github.com>
2024-01-11 10:14:54 +0900

    Update config.js

--- smartRoom_rest_api/config.js
+++ smartRoom_rest_api/config.js
@@ -2,14 +2,7 @@ const { initializeApp } = require ("firebase/app");
 const { getAuth } = require("firebase/auth");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCxl-0pbCJNOITw4IofyptWCpmbXVuZH5I",
-  authDomain: "esp8266firebase-a017f.firebaseapp.com",
-  databaseURL: "https://esp8266firebase-a017f-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "esp8266firebase-a017f",
-  storageBucket: "esp8266firebase-a017f.appspot.com",
-  messagingSenderId: "224421342334",
-  appId: "1:224421342334:web:7399eab88905df8ca97c2f",
-  measurementId: "G-BF3V0PYXCK"
+ --
 };
 
 
